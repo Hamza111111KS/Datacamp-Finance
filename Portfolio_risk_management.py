d117ba81626719e48cb3c4bce6e79e1cb6d1d52b
Meddad-Hamza <84096996+Hamza111111KS@users.noreply.github.com>
2022-12-30 10:36:44 +0100

    Update Portfolio_risk_management.py

--- Portfolio_risk_management.py
+++ Portfolio_risk_management.py
@@ -1,3 +1,14 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+matplotlib.rcParams['figure.figsize'] = (20, 10)
+
+file_path = "F:\\schulich\\datacamp\\applied finance\\2- Quantitative Risk Management in Python\\FourBanksFinancialCrisis.xlsx"
+# Read in the excelfile and parse dates
+portfolio = pd.read_excel(file_path, parse_dates = ['Date'], index_col='Date')
+portfolio
+
+
 # Select portfolio asset prices for the middle of the crisis, 2008-2009
 asset_prices = portfolio.loc['2008-01-01':'2009-12-31']
 
